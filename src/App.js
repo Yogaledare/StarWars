120cc54614ef04407a3811552a8a2f06298c2e4b
Yogaledare <marcus_uppstrom@hotmail.com>
2024-04-07 15:34:25 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import axios from 'axios';
+import SearchForm from "./SearchForm"; 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    
+    
+    const [characterName, setCharacterName] = useState(''); 
+    const [biometrics, setBiometrics] = useState(''); 
+    const [error, setError] = useState('');
+    const [loading, setLoading] =  useState(false); 
+    
+
+    
+    const fetchCharacterData = async (name) => {
+        setLoading(true); 
+        setError(''); 
+        
+        try {
+            const response = await axios.get(`https://www.swapi.tech/api/people/?name=${name}`)
+
+            console.log(response); 
+            
+            
+            
+        } catch (err) {
+            setError('Failed to fetch data. Please check the character name and try again.');
+            setBiometrics('');
+        } finally {
+            setLoading(false); 
+        }
+        
+        
+        
+        
+        
+        
+    }
+    
+
+    return (
+        <div className='container mt-5'>
+            <div className="row justify-content-center">
+                <div className="col-lg-8">
+                    <header className='my-5 '>
+                        <h1 className={'text-center'}>StarWars Character Biometrics Fetcher</h1>
+                    </header>
+
+                    <SearchForm
+                        setBiometrics={console.log("hi")}
+                    ></SearchForm>
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
+
+
+
+
+
+{/*<input */}
+{/*    type="text"*/}
+{/*    value={characterName}*/}
+{/*    onChange={(e) => setCharacterName(e.target.value)}*/}
+{/*    placeholder={'Enter Name'}*/}
+{/*/>*/}
+
+
+{/*<AddTodoForm*/}
+{/*    addTodo={addTodo}*/}
+{/*></AddTodoForm>*/}
+{/*<TodoList todos={todos} toggleComplete={toggleComplete} removeItem={removeItem}></TodoList>*/}
+{/*<ControlPanel*/}
+{/*    undoTodo={undoTodo}*/}
+{/*    removeAll={removeAll}*/}
+{/*></ControlPanel>*/}
+
---
+++ src/SearchForm.js
@@ -0,0 +1,33 @@
+﻿import {useState} from "react";
+
+
+const SearchForm = ({addTodo: setBiometrics}) => {
+    const [text, setText] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!text.trim()) return;
+        setBiometrics(text);
+        setText('');
+    }
+
+
+    return (
+        <form onSubmit={handleSubmit} className={'mb-3 d-flex'}>
+            <input
+                type="text"
+                className={'form-control me-2'}
+                placeholder="Enter Name"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+            <button
+                type="submit"
+                className={'btn btn-primary'}
+            > Search
+            </button>
+        </form>
+    )
+}
+
+export default SearchForm; 
