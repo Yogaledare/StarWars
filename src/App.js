1dd50e1869861f40cc837d336fd99fd48526c1c7
Yogaledare <marcus_uppstrom@hotmail.com>
2024-04-08 21:42:38 +0200

    added explanation paragraph

--- src/App.js
+++ src/App.js
@@ -11,8 +11,12 @@ function App() {
         <div className='container mt-5'>
             <div className="row justify-content-center">
                 <div className="col-lg-8">
-                    <header className='my-5 '>
-                        <h1 className={'text-center'}>StarWars Character Biometrics Fetcher</h1>
+                    <header className='mt-5 '>
+                        <h1 className={'text-center'}>StarWars Character Biometrics</h1>
+                        <p>
+                            Search on the name of a Star Wars character 
+                            (e.g., chewbacca, leia, luke) to retrieve biometrics info on this character. 
+                        </p>
                     </header>
 
                     <SearchForm
